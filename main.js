6838e383c1b1ac60bb0c71443a30ea0dc2663f79
Francis Adegbe <51995867+gudlyf2jef@users.noreply.github.com>
2022-09-10 13:45:28 +0100

    netflix old landing page built with html, css js

---
+++ main.js
@@ -0,0 +1,39 @@
+// code written by Francis Adegbe, 
+// please donnot remove this line as it signify credit to me
+// contact me +2347068473984
+// or Francismission61@gmail.com
+
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+	// Remove all show and border classes
+	removeBorder();
+	removeShow();
+	// Add border to current tab item
+	this.classList.add('tab-border');
+	// Grab content item from DOM
+	const tabContentItem = document.querySelector(`#${this.id}-content`);
+	// Add show class
+	tabContentItem.classList.add('show');
+}
+
+// Remove bottom borders from all tab items
+function removeBorder() {
+	tabItems.forEach(item => {
+		item.classList.remove('tab-border');
+	});
+}
+
+// Remove show class from all content items
+function removeShow() {
+	tabContentItems.forEach(item => {
+		item.classList.remove('show');
+	});
+}
+
+// Listen for tab item click
+tabItems.forEach(item => {
+	item.addEventListener('click', selectItem);
+});
